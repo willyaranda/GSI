9fd7d97d722fdfa574e1fa1131d02abe11029b82
Guillermo López <willyaranda@gmail.com>
2012-04-29 00:18:55 +0200

    Reverting to standard test suite

--- ej1/src/test/java/com/gsi/telecom/TestDaoMySql.java
+++ ej1/src/test/java/com/gsi/telecom/TestDaoMySql.java
@@ -244,7 +244,6 @@ public class TestDaoMySql extends TestCase {
 		// Add product to DB
 		con.createStatement().executeUpdate(getOrderAddProduct(product));
 		
-		System.out.println(product);
 		// Delete
 		productDao.delete(product);
 
